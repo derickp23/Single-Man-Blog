3d77b030d20fd7fb3fd9f23d91ffc6c21e7682c8
derickp23 <91038353+derickp23@users.noreply.github.com>
2021-12-04 22:10:16 -0500

    added form code

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+function myFunction() {
+    var x = document.getElementById("Relationshipquotes");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";}}
+   
+      function myFunction(){
+        var x = document.getElementById("Inspirationalquotes");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }}
+
+function validateForm()
+        {
+        var x=document.forms["nameofform"]["emailid"].value;
+        var atpos=x.indexOf("@");
+        var dotpos=x.lastIndexOf(".");
+        if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+         {
+         alert("Enter a Valid Email Address");
+         return false;
+         }
+        }
+
+       
+
+  
 
   
